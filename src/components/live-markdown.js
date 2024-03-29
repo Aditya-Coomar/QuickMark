357a54f7ec1e49556b744e65111e142802fefa0d
Aditya-Coomar <adi.coomar04@gmail.com>
2024-03-30 01:52:19 +0530

    feat: add utilities
    1. Add copy buttons for Markdown and HTML type copy
    2. Add buttons for appending horizontal rule, checked list, block-quote and table.

--- src/components/live-markdown.js
+++ src/components/live-markdown.js
@@ -1,49 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import copy from "copy-to-clipboard";
+import { toast } from "react-toastify";
 
 export default function LiveMarkdown() {
-
   const buttonType = [
     { index: 1, name: "heading" },
     { index: 2, name: "bold" },
     { index: 3, name: "italic" },
+    { index: 4, name: "list-check" },
     { index: 5, name: "strikethrough" },
     { index: 6, name: "code" },
     { index: 7, name: "link" },
     { index: 8, name: "image" },
-    { index: 9, name: "trash-can" },
+    { index: 9, name: "quote-left" },
+    { index: 10, name: "ruler-horizontal" },
+    { index: 12, name: "table" },
+    { index: 11, name: "trash-can" },
   ];
-  
+
   const [markdownInput, setMarkdownInput] = useState("");
-  const handleMarkdownButton = (name) => {
+  const [html, setHtml] = useState("");
+
+  function handleMarkdownButton(name) {
     if (name === "bold") {
-      setMarkdownInput(markdownInput + "**Bold Text**");
+      setMarkdownInput(markdownInput + " **Bold Text**");
     } else if (name === "italic") {
-      setMarkdownInput(markdownInput + "*Italic Text*");
+      setMarkdownInput(markdownInput + " *Italic Text*");
     } else if (name === "strikethrough") {
-      setMarkdownInput(markdownInput + "~~Strikethrough Text~~");
+      setMarkdownInput(markdownInput + " ~~Strikethrough Text~~");
     } else if (name === "code") {
       setMarkdownInput(
         markdownInput +
-          "```\n function mycode(x) { \n this.x = test; \n } \n```"
+          " \n ```\n function mycode(x) { \n this.x = test; \n } \n```"
       );
     } else if (name === "link") {
-      setMarkdownInput(markdownInput + "[This is a sample link change link to change image](https://sourcebae.com)");
+      setMarkdownInput(
+        markdownInput +
+          " \n [This is a sample link change link to change image](https://sourcebae.com)"
+      );
     } else if (name === "image") {
       setMarkdownInput(
         markdownInput +
-          "![This is a sample image change url to change image](https://sourcebae.com/blog/wp-content/uploads/2023/08/6.png)"
+          " \n ![This is a sample image change url to change image](https://sourcebae.com/blog/wp-content/uploads/2023/08/6.png)"
       );
     } else if (name === "heading") {
       setMarkdownInput(
         markdownInput +
-          "# Heading 1 \n ## Heading 2 \n ### Heading 3 \n #### Heading 4 \n ##### Heading 5 \n ###### Heading 6"
+          " \n # Heading 1 \n ## Heading 2 \n ### Heading 3 \n #### Heading 4 \n ##### Heading 5 \n ###### Heading 6"
       );
     } else if (name === "trash-can") {
       setMarkdownInput("");
+    } else if (name === "list-check") {
+      setMarkdownInput(
+        markdownInput +
+          " \n - [ ] List item 1 (Unchecked) \n - [x] List item 2 (Checked)"
+      );
+    } else if (name === "quote-left") {
+      setMarkdownInput(markdownInput + " \n > This is a sample quote");
+    } else if (name === "ruler-horizontal") {
+      setMarkdownInput(markdownInput + " \n ---");
+    } else if (name === "table") {
+      setMarkdownInput(
+        markdownInput +
+          " \n | column 1 | column 2 | column 3 |   \n | ---- | --- | --- |  \n | row 1 | row 1 | row 1 |  \n | row 2 | row 2 | row 2 |  \n | row 3 | row 3 | row 3 |  \n"
+      );
     }
-  };
+  }
+
+  function copyMarkdownText() {
+    if (markdownInput === "") {
+      toast.info("Nothing to Copy!");
+      return;
+    }else{
+    copy(markdownInput);
+    toast.success("Copied Markdown Text!");}
+  }
+
+  function copyHtmlText() {
+    if (html === "") {
+      toast.info("Nothing to Copy!");
+      return;
+    }else{
+    copy(html);
+    toast.success("Copied as HTML");}
+  }
+
+  useEffect(() => {
+    const el = document.querySelector(".preview-output");
+    if (el) {
+      const mdHTML = el.innerHTML;
+      setHtml(mdHTML);
+    }
+  }, [markdownInput]);
 
   return (
     <div className="flex flex-wrap gap-4 justify-center p-5 mt-4">
@@ -55,7 +105,7 @@ export default function LiveMarkdown() {
           </span>
         </div>
         <div className="bg-slate-950 text-sm sm:text-base md:text-lg">
-          <div className="grid grid-cols-8 items-stretch gap-0 bg-gradient-to-r from-teal-600 to-cyan-600 bg-clip-text text-transparent font-bold">
+          <div className="grid grid-cols-6 items-stretch gap-0 bg-gradient-to-r from-teal-600 to-cyan-600 bg-clip-text text-transparent font-bold">
             {buttonType.map((button) => (
               <button
                 className="p-1 text-center border-2 border-slate-900 hover:bg-gradient-to-r hover:from-teal-700 hover:to-cyan-700 hover:text-slate-950"
@@ -71,11 +121,21 @@ export default function LiveMarkdown() {
                 ></i>
               </button>
             ))}
+            <button
+              className="p-1 col-span-3 text-center border-2 border-slate-900 hover:bg-gradient-to-r hover:from-teal-700 hover:to-cyan-700 hover:text-slate-950"
+              type="button"
+              title="copy-as-markdown"
+              onClick={copyMarkdownText}
+              ><span><i class="fa-brands fa-markdown"></i>&nbsp;</span> Copy Markdown Text</button>
+            <button 
+            className="p-1 col-span-3 text-center border-2 border-slate-900 hover:bg-gradient-to-r hover:from-teal-700 hover:to-cyan-700 hover:text-slate-950" 
+            type="button" title="copy-as-html" onClick={copyHtmlText}><span><i class="fa-brands fa-html5"></i>&nbsp;</span> Copy as HTML</button>
+            
           </div>
         </div>
         <textarea
           autoFocus
-          className="w-full h-[300px] md:h-[600px] p-3 bg-slate-900 text-white overflow-auto font-[Montserrat]"
+          className="w-full h-[300px] md:h-[600px] p-3 bg-black text-white overflow-auto font-[Montserrat]"
           value={markdownInput}
           onChange={(e) => setMarkdownInput(e.target.value)}
           placeholder="Enter markdown here..."
@@ -88,8 +148,11 @@ export default function LiveMarkdown() {
             Preview{" "}
           </span>
         </div>
-        <div className="w-full h-[340px] md:h-[640px] bg-slate-900 text-white overflow-auto font-[Montserrat] p-3 text-base">
-          <ReactMarkdown className="prose-base" remarkPlugins={[remarkGfm]}>
+        <div className="w-full h-[340px] md:h-[715px] bg-black text-white overflow-auto font-[Montserrat] p-3 text-base mt-[2px]">
+          <ReactMarkdown
+            className="prose-base preview-output"
+            remarkPlugins={[remarkGfm]}
+          >
             {markdownInput}
           </ReactMarkdown>
         </div>
@@ -97,8 +160,6 @@ export default function LiveMarkdown() {
     </div>
   );
 }
-
-
 
 /* 
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ToastContainer
+      position="bottom-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
     <App />
   </React.StrictMode>
 );
